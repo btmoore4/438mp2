f60e2ae2249db9fbccc7f1a73b124e968f92138d
Ben Moore <btmoore95@gmail.com>
2016-11-15 01:12:51 -0600

    Fixed args

--- testclient.py
+++ testclient.py
@@ -7,13 +7,14 @@ host = 'localhost'
 port = 1111
 filename="tests/peter3.txt"
 count = 0 
+petertests = {}
 petertests[0] = "tests/peter.txt"
 petertests[1] = "tests/peter.txt"
 petertests[2] = "tests/peter.txt"
 
 if len(sys.argv) == 2:
-    host = sys.argv[0]
-    filename = petertests[sys.argv[1]-1]
+    host = str(sys.argv[0])
+    filename = petertests[int(sys.argv[1])-1]
 elif len(sys.argv) == 1:
     host = sys.argv[0]
 
