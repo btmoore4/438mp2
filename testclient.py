514402c283d1af945f5764d76414d8ef8be5f632
Ben Moore <btmoore95@gmail.com>
2016-11-15 01:10:52 -0600

    Added args to testclient

--- testclient.py
+++ testclient.py
@@ -1,11 +1,21 @@
 from transport import MP2Socket, MP2SocketError
 from sender import PKT
 import time
+import sys
 
 host = 'localhost'
 port = 1111
 filename="tests/peter3.txt"
 count = 0 
+petertests[0] = "tests/peter.txt"
+petertests[1] = "tests/peter.txt"
+petertests[2] = "tests/peter.txt"
+
+if len(sys.argv) == 2:
+    host = sys.argv[0]
+    filename = petertests[sys.argv[1]-1]
+elif len(sys.argv) == 1:
+    host = sys.argv[0]
 
 print "testclient.py - Initiate Socket and Connection"
 f = open(filename, 'rb')
